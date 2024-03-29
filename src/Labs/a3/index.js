9e6907b5e6708adae1d63ce7e4e0be58cb0aee35
sougata1996 <sougatamukherjeejsr@gmail.com>
2024-03-29 17:02:30 -0400

    Fixing build error

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,6 +1,5 @@
 import JavaScript from "./Javascript";
 import PathParameters from "./PathParameters";
-import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./Todo/TodoList";
@@ -8,10 +7,10 @@ import { useSelector } from "react-redux";
 
 function Assignment3() {
   const { todos } = useSelector((state) => state.todosReducer);
-    return (
-      <div >
-        <h1>Assignment 3</h1>
-        <ul className="list-group">
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <ul className="list-group">
         {todos.map((todo) => (
           <li className="list-group-item" key={todo.id}>
             {todo.title}
@@ -19,16 +18,12 @@ function Assignment3() {
         ))}
       </ul>
 
-        <TodoList/>
-        <ConditionalOutput/>
-        <Styles/>
-        <Classes/>
-        <PathParameters/>
-        <JavaScript/>
-        
-      </div>
-    );
-  }
-  export default Assignment3;
-  
-  
+      <TodoList />
+      <ConditionalOutput />
+      <Styles />
+      <PathParameters />
+      <JavaScript />
+    </div>
+  );
+}
+export default Assignment3;
